cbfcb34cbdd850fdc095edcc8b7af62343dd3a1f
xRain <xrain@simcu.com>
2017-05-19 16:38:20 +0800

    fix logs stdout

--- files/run.sh
+++ files/run.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 php-fpm7
 nginx
-echo "" > /home/storage/logs/lumen.log
-echo "" > /home/storage/logs/laravel.log
-chmod -R 777 /home/storage
-tail -f /home/storage/logs/lumen.log &
-tail -f /home/storage/logs/laravel.log
+tail -f /var/log/nginx/error.log
